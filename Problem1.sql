44c85b9d139f0177d72b0721b72ba21491ee89ff
jsonaguas <jaguas43@gmail.com>
2024-06-30 23:37:09 -0500

    upload

---
+++ Problem1.sql
@@ -0,0 +1,36 @@
+-- TASK 1 & TASK 3
+CREATE DATABASE music_library;
+
+USE music_library;
+
+
+-- Create the Albums table
+CREATE TABLE albums (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(150),
+  artist_id INT,
+  genre_id INT,
+  release_year INT,
+  total_tracks INT,
+  FOREIGN KEY (artist_id) REFERENCES artists(id),
+  FOREIGN KEY (genre_id) REFERENCES genres(id)
+);
+
+-- Create the Genres table
+CREATE TABLE genres (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100)
+);
+
+-- Create the Artists table
+CREATE TABLE artists (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(150),
+  genre_id INT,
+  label VARCHAR(80),
+  FOREIGN KEY (genre_id) REFERENCES genres(id)
+);
+-- TASK 2
+ALTER TABLE albums
+add column total_duration INT;
+
